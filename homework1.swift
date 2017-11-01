97dbcd8ba7df0e63d5b2908d2170623a16ebc1ee
Taha Demir <tahadmr.1995@gmail.com>
2017-11-01 11:33:39 +0300

    Update homework1.swift

--- homework1.swift
+++ homework1.swift
@@ -1,4 +1,4 @@
-
+//1) Write two functions that overload of each others
 class overloadFunctionExample
 {    
     func overload(ad: String) -> String
@@ -9,7 +9,7 @@ class overloadFunctionExample
     
     func overload(sene: Int) -> String
     {
-        let dogum = "Dogum Yýlýnýz : " + String(sene)
+        let dogum = "Dogum Yılınız : " + String(sene)
         return "\(dogum)"
     }
 }
@@ -22,44 +22,45 @@ debugPrint(example1_1)
 
 let example1_2 = example1.overload(sene: 1995)
 debugPrint(example1_2)
-       
-
+   
+//2) Write a function which has default parameter value
 class defaultParameterValueExample {
-	func iosLecture(cevap: String, onayAl: String = "Yemek yemek istermisinz?")
-    {
-        debugPrint("\(onayAl): \(cevap)")
-    }
+   func iosLecture(cevap: String, onayAl: String = "Yemek yemek istermisinz?")
+   {
+      debugPrint("\(onayAl): \(cevap)")
+   }
 }
 
 let example2 = defaultParameterValueExample()
 
-example2.iosLecture(cevap: "Hayýr")
+example2.iosLecture(cevap: "Hayır")
 example2.iosLecture(cevap: "Evet", onayAl: "Içecek birsey istermisiniz?")
 
+//3) Write a function which has one paremeter and one variadic parameter
 class variadicParameterExample {
-	func sayýAl(sayý: Int ..., defaultDeger: String = "Girdiginiz sayilar : ")
-    {      
-        debugPrint("\(defaultDeger) \(sayý)")
-    }
+   func sayiAl(sayi: Int ..., defaultDeger: String = "Girdiginiz sayilar : ")
+   {     
+      debugPrint("\(defaultDeger) \(sayi)")
+   }
 }
 
 let example3 = variadicParameterExample()
 
-example3.sayýAl(sayý: 3,4,5)
+example3.sayiAl(sayi: 3,4,5)
 
-
+//4) Write a function which has ignored external parameter name.
 class ignoredExternalParameterExample{
-	func ignoredExample(_ avg: Int, _: String) 
+    func ignoredExample(_ avg: Int, _: String) 
+    {
+	if (avg  >= 60)
 	{
-		if (avg  >= 60)
-		{
-			debugPrint("Dersten geçtiniz")
-		}
-		else 
-		{
-			debugPrint("Bir dahaki dönem yine birlikteyiz :D")
-		}
-	}	
+	    debugPrint("Dersten geçtiniz")
+	}
+	else 
+	{
+	    debugPrint("Bir dahaki dönem yine birlikteyiz :D")
+	}
+    }	
 }
 
 let example4 = ignoredExternalParameterExample()
@@ -67,18 +68,17 @@ let example4 = ignoredExternalParameterExample()
 example4.ignoredExample(72, "Buranin bi etkisi yok")
 example4.ignoredExample(55, "Ama burayi eklemezsen hata verir..")
 
-
-func functionInsideFunction(_ h: Int, _ w: Int) {
-    
+//5) Write a function inside a function.
+func functionInsideFunction(_ h: Int, _ w: Int) {    
     func printAreaOfREctangle() {
-		let a = h * w
+	let a = h * w
         print("Area: \(a)")
     }
     printAreaOfREctangle()
 }
 functionInsideFunction(4, 5)
 
-
+//6) Write a recursive function.
 func recursiveFunctionExample(_ deger: Int) {
     debugPrint(deger)
     if deger > 1 {
@@ -87,7 +87,7 @@ func recursiveFunctionExample(_ deger: Int) {
 }
 
 recursiveFunctionExample(5)
-
+//7) Write typealias for a function type which has different parameter types.
 typealias typealiasExample = (Int, Int) -> Int
 
 let multiplyTwoNumbers: typealiasExample = { deger1, deger2 in
@@ -95,7 +95,7 @@ let multiplyTwoNumbers: typealiasExample = { deger1, deger2 in
 }
 
 debugPrint("Sonuc  : \(multiplyTwoNumbers(4,5))")
-
+//8) Pass function as a value to another function.
 func additionNumbers(getNumbers: (_ a: Int, _ b:Int) -> ()) {
     getNumbers(4, 5)
 }
